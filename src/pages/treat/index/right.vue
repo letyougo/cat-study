1ba0cdcb647a46cbe0fa997e9a1fd1f507530392
letyougo <surui123>
2018-12-02 17:25:56 +0800

    bbb

--- src/pages/treat/index/right.vue
+++ src/pages/treat/index/right.vue
@@ -2,14 +2,30 @@
     <div class="right" v-show="list.length>0">
         <div class="suggest dia-item">
             <div class="top" style="padding-left: 0;padding-right: 0;display: flex;justify-content: space-between;align-items: center">
-                <div class="title-tip" style="padding: 0;">疑似诊断提示</div>
+                <div class="title-tip" style="padding: 0;">
+                  <div style="font-size: 16px">
+                      疑似诊断提示
+                  </div>
+                 
+                </div>
                 <div>
 
                     <el-checkbox label="全选" @change="kdmodel=kdexams" v-if="kdmodel.length !== kdexams.length"></el-checkbox>
                       <el-checkbox label="全选" @change="kdmodel=[]" v-else></el-checkbox>
                 </div>
             </div>
-            <div >
+
+            <div style="padding: 10px 0;display: flex;justify-content: space-between;align-items: center">
+              <div>症状推荐检查</div>
+              <span  @click="open=true"  v-if="!open">
+                  <el-tag size="mini" style="background: #ffffff">收起</el-tag>
+              </span>
+              <span v-else @click="open=false">
+                  <el-tag size="mini" style="background: #ffffff" >展开</el-tag>
+              </span>
+            </div>
+
+            <div v-show="!open">
               <el-form >
                   <el-checkbox-group v-model="kdmodel">
                     <div style="margin: 6px 0" v-for="(item,index) in kdexams" :key="item.item"  >
@@ -86,14 +102,14 @@
 
                     <p>
                         <span v-for="op in item.symptoms"  :key="op" :label="op" size="mini'" style="padding: 4px 6px;display: inline-block">
-                            <el-tag type="primary" v-if="item.matchSymptoms.includes(op)">{{op}}</el-tag>
+                            <el-tag style="background: #ffffff" type="primary" v-if="item.matchSymptoms.includes(op)">{{op}}</el-tag>
                             <el-tag v-else style="background: #ffffff;color: #333333;border-color: #dcdfe6">{{op}}</el-tag>
                         </span>
                     </p>
                     <p style="margin: 10px 0">行为&查体</p>
                     <p>
                       <span v-for="op in item.checks"  :key="op" :label="op" size="mini'" style="padding: 4px 6px;display: inline-block">
-                        <el-tag type="primary" v-if="item.matchSymptoms.includes(op)">{{op}}</el-tag>
+                        <el-tag  style="background: #ffffff" type="primary" v-if="item.matchSymptoms.includes(op)">{{op}}</el-tag>
                         <el-tag v-else style="background: #ffffff;color: #333333;border-color: #dcdfe6">{{op}}</el-tag>
                       </span>
                     </p> 
@@ -203,6 +219,7 @@ export default {
   },
   data () {
     return {
+      open: false,
       kdexams: [],
       kdmodel: [],
       names: '',
